313773b917dccc4985b573cd30e3e355ae63d3bf
vbocw <97356369+vbocw@users.noreply.github.com>
2022-01-10 21:07:35 +0530

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,40 @@
+console.log("test");
+var tableData = [
+    //dictionary
+];
+var tableObj;
+for (i = 1; i <= 50; i++) {
+    tableData.push({
+      id: i,
+      slno: i,
+      subjectName: "Physics",
+      docLink:"Test",
+      dept: "Physics",
+      courseLevel: "Post Graduate",
+      InstructorName: "Prof. BCG",
+      semYear: "Semester I, 2022",
+      contributorName: "Arijit Das",
+      description: "Sample Doc"
+    });
+  }
+DataTableObject = new Tabulator(document.getElementById("main-table"), {
+data: tableData,
+height: "1000px",
+addRowPos: "bottom",
+columns:[
+    {title:"#", field:"slno", width:50, headerSort:false},
+    {title:"Course", field:"subjectName", width:"150", hozAlign:"right", headerSort:true},
+    {title:"Subject", field:"subjectName", width:"150", hozAlign:"right"},
+    {title:"Topic", field:"subjectName", width:"150", hozAlign:"right", headerSort:false},
+    {title:"Description", field:"description",width:"150", hozAlign:"center", headerSort:false},  
+    {title:"Link", field:"docLink", width:"100", hozAlign:"right", headerSort:false},
+    {title:"Contributed by", field:"contributorName",width:"150", hozAlign:"center"},
+    {title:"Department", field:"dept",width:"150", hozAlign:"center"},
+    {title:"Level", field:"courseLevel",width:"100", hozAlign:"center"},
+    {title:"Instructor", field:"InstructorName",width:"100", hozAlign:"center"},
+    {title:"Year & Semester", field:"semYear",width:"100", hozAlign:"center", headerSort:false},
+    {title:"DocID", field:"id", width:"100"},
+    
+],
+});
+

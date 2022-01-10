31f6ecefa8987a6f88b3be3f422ccd23860782a4
VB OpenCourseWire <97356369+vbocw@users.noreply.github.com>
2022-01-10 22:50:28 +0530

    dataArray.js
    Data for table

---
+++ dataArray.js
@@ -0,0 +1,40 @@
+var tableData = [
+    {
+      id: "Phys1",
+      slno: "1",
+      subjectName: "Physics",
+      docLink:"Test",
+      dept: "Physics",
+      courseLevel: "Post Graduate",
+      InstructorName: "Prof. BCG",
+      semYear: "Semester I, 2022",
+      contributorName: "Arijit Das",
+      description: "Sample Doc"
+    },
+    {
+        id: "Phys2",
+        slno: "2",
+        subjectName: "Physics",
+        docLink:"Test",
+        dept: "Physics",
+        courseLevel: "Post Graduate",
+        InstructorName: "Prof. BCG",
+        semYear: "Semester I, 2022",
+        contributorName: "Arijit Das",
+        description: "Sample Doc"
+      },
+      {
+        id: "Phys2",
+        slno: "2",
+        subjectName: "Physics",
+        docLink:"Test",
+        dept: "Physics",
+        courseLevel: "Post Graduate",
+        InstructorName: "Prof. BCG",
+        semYear: "Semester I, 2022",
+        contributorName: "Arijit Das",
+        description: "Sample Doc"
+      },
+
+
+]
